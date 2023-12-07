147fe7435c41e381dd4e9b39bbda71c8382e148b
pmleconte <pascal.leconte@nordnet.fr>
2023-12-07 10:52:27 +0100

    2.3.4
    Frontend : check noscript and switcheditor.js

--- mod_site_switcheditor/tmpl/default.php
+++ mod_site_switcheditor/tmpl/default.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * @package    Switch Editor
- * @subpackage mod_switcheditor
- * @copyright  Copyright (C) 2021 ConseilGouz. All rights reserved.
+ * @subpackage mod_switcheditor site
+ * @copyright  Copyright (C) 2023 ConseilGouz. All rights reserved.
  * From anything-digital.com Switch Editor
- * @license    GNU/GPLv2
+ * @license    GNU/GPLv3
  */
 // no direct access
 defined('_JEXEC') or die;
@@ -12,6 +12,7 @@ use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ModuleHelper;
 use Joomla\CMS\Version;
+use Joomla\CMS\Language\Text;
 
 // Joomla 4.0 custom field conflict 
 if (($app->input->get('option') == 'com_fields') && ($app->input->get('view') == 'field') && ($app->input->get('layout') == 'edit')) return;
@@ -19,9 +20,21 @@ $value = Factory::getUser()->getParam('editor');
 /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
 $wa = Factory::getDocument()->getWebAssetManager();
 $wa->registerAndUseStyle('switcheditor','media/switcheditor/css/switcheditor_j4.css');
+if (!is_file(JPATH_ROOT . '/media/switcheditor/js/switcheditor.js')) {
+	echo '<div class="alert alert-danger" role="alert">';
+	echo Text::_('MOD_SWITCHEDITOR_NOJS');
+	echo '</div>';
+	return;
+}
 $wa->registerAndUseScript('switcheditor','media/switcheditor/js/switcheditor.js');
 
 ?>
+<noscript>
+    <div class="alert alert-danger" role="alert">
+        <?php echo Text::_('MOD_SWITCHEDITOR_NOSCRIPT'); ?>
+    </div>
+</noscript>
+
 <div class="adEditorFormBox btn-group">
 	<form name="adEditorFormSite">
 		<?php echo str_replace(' id="adEditor"', '', HTMLHelper::_('select.genericlist', $options,'adEditor', ' class="adEditor chzn-done" data-chosen="done"','element', 'name',$value)); ?>
